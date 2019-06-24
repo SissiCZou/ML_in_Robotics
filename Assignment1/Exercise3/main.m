fddd27bb7feb56b1c2dcce0e480cdc36d7c1312e
chenxizou2013@gmail.com <chenxi.zou@tum.de>
2019-06-24 23:54:27 +0200

    change in main file

--- Assignment1/Exercise3/main.m
+++ Assignment1/Exercise3/main.m
@@ -1,6 +1,6 @@
 %%
 clc
-clear all
+clear all;
 
 %%
 % load the data
@@ -17,91 +17,45 @@ plot(num_rep, gesture_o, gesture_l, gesture_x, init_cluster_l, init_cluster_o, i
 %k-means, reference: http://www.christianherta.de/kmeans.php
 
 % for 1-th data points, sort the color
-data = squeeze(gesture_o(:,1,:)); % 60*3
+data = reshape(gesture_o, [600,3]); % 60*3
 dim_data = length(data(1,:)); %3
 num_data = length(data(:,1)); %60
 
-%init centriods randomly
-
-min_data = min(data);
-max_data = max(data);
-diff_data = max_data - min_data; 
-% every row is a centriod
-centriod = ones(num_clus, dim_data).*rand(num_clus, dim_data);
-for i = 1:1:num_clus
-    centriod(i,:) = centriod(i,:).*diff_data;
-    centriod(i,:) = centriod(i,:) + min_data;
-end 
 
 % no stopping at start
-pos_diff = 1.0;
+decrement = inf;
+tolerance = 10e-6;
+J_old = inf;
+steps = 0;
 
-while pos_diff > 0:
-    
+while decrement > tolerance
+    dist_min = zeros(num_data,1);
+    labels = zeros(num_data,1);
     % E-step
-    assign = [];
-    
     % assign each datapoint to the closest centroid
     for d = 1:num_data
-        min_diff = data(d,:) - centriod(1,:);
-        min_diff = min_diff*min_diff'; % distance^2
-        cur_assign = 1;
+        dist = zeros(num_clus,1);
         
-        for c = 2:num_clus
-            diff2c = (data(d,:) - centriod(c,:));
-            diff2c = diff2c*diff2c';
-            if min_diff >= diff2c
-                cur_assign = c;
-                min_diff = diff2c;
-            end
+        for c = 1:num_clus
+            dist(c) = (data(d,:) - init_cluster_l(c,:))*(data(d,:) - init_cluster_l(c,:))';
         end 
         
         % assign the d-th data point
-        assign = [assign; cur_assign];
+        [dist_min(d), labels(d)] = min(dist);
     end 
-    % when to stop
-%% plot
-% why plot is not working???
-function plot(num_rep, gesture_o, gesture_l, gesture_x, init_cluster_l, init_cluster_o, init_cluster_x)
-    figure(1);
-    color = ['b+', 'k+', 'r+', 'g+', 'm+', 'y+', 'c+'];
-    subplot(1,3,1);
-    for i = 1:num_rep
-        plot3(gesture_l(:,i,1), gesture_l(:,i,2), gesture_l(:,i,3));
-        hold on;
+    % M-step
+    for c = 1:num_clus
+        init_cluster_l(c,:) = mean(data(labels == c, :));
     end
-    hold on;
-    for i = 1:7
-        scatter3(init_cluster_l(i,1), init_cluster_l(i,2), init_cluster_l(i,3), color(i), 'LineWidth', 2);
-    end
-    title('gesture l');
-    hold off;
-
-    subplot(1,3,2);
-    for i = 1:num_rep
-        plot3(gesture_o(:,i,1), gesture_o(:,i,2), gesture_o(:,i,3));
-        hold on;
-    end
-    hold on;
-    for i = 1:7
-        scatter3(init_cluster_o(i,1), init_cluster_o(i,2), init_cluster_o(i,3), color(i), 'LineWidth', 2);
-    end
-    title('gesture o');
-
-    subplot(1,3,3);
-    for i = 1:num_rep
-        plot3(gesture_x(:,i,1), gesture_x(:,i,2), gesture_x(:,i,3));
-        hold on;
-    end
-    hold on;
-    for i = 1:7
-        scatter3(init_cluster_x(i,1), init_cluster_x(i,2), init_cluster_x(i,3), color(i), 'LineWidth', 2);
-    end
-    title('gesture x');
-end
-
-%% 
-% distance to the reference points
+    % total distortion
+    J = sum(dist_min);
+    
+    % dicrement of distortion
+    decrement = J_old - J;
+    J_old = J;
+    steps = steps + 1;
+end 
+    
 
 
 
---
+++ Assignment1/Exercise3/plot_data.m
@@ -0,0 +1,37 @@
+function plot_data(num_rep, gesture_o, gesture_l, gesture_x, init_cluster_l, init_cluster_o, init_cluster_x)
+    figure(1);
+    color = ['b+', 'k+', 'r+', 'g+', 'm+', 'y+', 'c+'];
+    subplot(1,3,1);
+    for i = 1:num_rep
+        plot3(gesture_l(:,i,1), gesture_l(:,i,2), gesture_l(:,i,3));
+        hold on;
+    end
+    hold on;
+    for i = 1:7
+        scatter3(init_cluster_l(i,1), init_cluster_l(i,2), init_cluster_l(i,3), color(i), 'LineWidth', 2);
+    end
+    title('gesture l');
+    hold off;
+
+    subplot(1,3,2);
+    for i = 1:num_rep
+        plot3(gesture_o(:,i,1), gesture_o(:,i,2), gesture_o(:,i,3));
+        hold on;
+    end
+    hold on;
+    for i = 1:7
+        scatter3(init_cluster_o(i,1), init_cluster_o(i,2), init_cluster_o(i,3), color(i), 'LineWidth', 2);
+    end
+    title('gesture o');
+
+    subplot(1,3,3);
+    for i = 1:num_rep
+        plot3(gesture_x(:,i,1), gesture_x(:,i,2), gesture_x(:,i,3));
+        hold on;
+    end
+    hold on;
+    for i = 1:7
+        scatter3(init_cluster_x(i,1), init_cluster_x(i,2), init_cluster_x(i,3), color(i), 'LineWidth', 2);
+    end
+    title('gesture x');
+end
