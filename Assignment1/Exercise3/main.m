2a2e99ec1c8ff03f0c7ed46fa56f7dc6489bb4e9
Zou <ga78woy@linux77.clients.eikon.tum.de>
2019-06-25 21:52:21 +0200

    finish ex3

--- Assignment1/Exercise3/main.m
+++ Assignment1/Exercise3/main.m
@@ -7,103 +7,52 @@ clear all
 load('gesture_dataset.mat')
 num_pos = size(gesture_l,1);
 num_rep = size(gesture_l,2);
-num_clus = size(init_cluster_l,1);
 
 %%
 % run functions defined below
-plot(num_rep, gesture_o, gesture_l, gesture_x, init_cluster_l, init_cluster_o, init_cluster_x)
+dataplot(num_rep, gesture_o, gesture_l, gesture_x, init_cluster_l, init_cluster_o, init_cluster_x)
+
 
 %%
-%k-means, reference: http://www.christianherta.de/kmeans.php
+% k-means
+%tic
+[steps_gesture_l, labels_gesture_l] = kmeansclus(gesture_l, init_cluster_l);
+[steps_gesture_o, labels_gesture_o] = kmeansclus(gesture_o, init_cluster_o);
+[steps_gesture_x, labels_gesture_x] = kmeansclus(gesture_x, init_cluster_x);
+%toc
+%%
+% plot a)
+figure(1)
+plotclus(gesture_l, labels_gesture_l);
+title('Kmeans clustering for gesture l');
+saveas(gcf, 'a11.png');
+figure(2)
+plotclus(gesture_o, labels_gesture_o);
+title('Kmeans clustering for gesture o');
+saveas(gcf, 'a12.png');
+figure(3)
+plotclus(gesture_x, labels_gesture_x);
+title('Kmeans clustering for gesture x');
+saveas(gcf, 'a13.png');
 
-% for 1-th data points, sort the color
-data = squeeze(gesture_o(:,1,:)); % 60*3
-dim_data = length(data(1,:)); %3
-num_data = length(data(:,1)); %60
-
-%init centriods randomly
-
-min_data = min(data);
-max_data = max(data);
-diff_data = max_data - min_data; 
-% every row is a centriod
-centriod = ones(num_clus, dim_data).*rand(num_clus, dim_data);
-for i = 1:1:num_clus
-    centriod(i,:) = centriod(i,:).*diff_data;
-    centriod(i,:) = centriod(i,:) + min_data;
-end 
-
-% no stopping at start
-pos_diff = 1.0;
-
-while pos_diff > 0:
-    
-    % E-step
-    assign = [];
-    
-    % assign each datapoint to the closest centroid
-    for d = 1:num_data
-        min_diff = data(d,:) - centriod(1,:);
-        min_diff = min_diff*min_diff'; % distance^2
-        cur_assign = 1;
-        
-        for c = 2:num_clus
-            diff2c = (data(d,:) - centriod(c,:));
-            diff2c = diff2c*diff2c';
-            if min_diff >= diff2c
-                cur_assign = c;
-                min_diff = diff2c;
-            end
-        end 
-        
-        % assign the d-th data point
-        assign = [assign; cur_assign];
-    end 
-    % when to stop
-%% plot
-% why plot is not working???
-function plot(num_rep, gesture_o, gesture_l, gesture_x, init_cluster_l, init_cluster_o, init_cluster_x)
-    figure(1);
-    color = ['b+', 'k+', 'r+', 'g+', 'm+', 'y+', 'c+'];
-    subplot(1,3,1);
-    for i = 1:num_rep
-        plot3(gesture_l(:,i,1), gesture_l(:,i,2), gesture_l(:,i,3));
-        hold on;
-    end
-    hold on;
-    for i = 1:7
-        scatter3(init_cluster_l(i,1), init_cluster_l(i,2), init_cluster_l(i,3), color(i), 'LineWidth', 2);
-    end
-    title('gesture l');
-    hold off;
-
-    subplot(1,3,2);
-    for i = 1:num_rep
-        plot3(gesture_o(:,i,1), gesture_o(:,i,2), gesture_o(:,i,3));
-        hold on;
-    end
-    hold on;
-    for i = 1:7
-        scatter3(init_cluster_o(i,1), init_cluster_o(i,2), init_cluster_o(i,3), color(i), 'LineWidth', 2);
-    end
-    title('gesture o');
-
-    subplot(1,3,3);
-    for i = 1:num_rep
-        plot3(gesture_x(:,i,1), gesture_x(:,i,2), gesture_x(:,i,3));
-        hold on;
-    end
-    hold on;
-    for i = 1:7
-        scatter3(init_cluster_x(i,1), init_cluster_x(i,2), init_cluster_x(i,3), color(i), 'LineWidth', 2);
-    end
-    title('gesture x');
-end
-
-%% 
-% distance to the reference points
-
-
-
-
-
+%%
+% uniform-random binary split
+tic
+[clus_center_l, labels_gesture_l] = nubsplit(gesture_l);
+[clus_center_o, labels_gesture_o] = nubsplit(gesture_o);
+[clus_center_x, labels_gesture_x] = nubsplit(gesture_x);
+toc
+%%
+% plot b)
+figure(1)
+plotclus(gesture_l, labels_gesture_l);
+title('nubsplit clustering for gesture l');
+saveas(gcf, 'b21.png');
+figure(2)
+plotclus(gesture_o, labels_gesture_o);
+title('nubsplit clustering for gesture o');
+saveas(gcf, 'b22.png');
+figure(3)
+plotclus(gesture_x, labels_gesture_x);
+title('nubsplit clustering for gesture x');
+saveas(gcf, 'b23.png');
